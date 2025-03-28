5396cab5771a9dfa4a0b6c6db54d690c76194d39
getinthere <getinthere@naver.com>
2025-03-28 14:08:12 +0900

    유저 의ã조존성 연결

--- src/main/java/com/metacoding/storev2/user/UserRepository.java
+++ src/main/java/com/metacoding/storev2/user/UserRepository.java
@@ -1,4 +1,11 @@
 package com.metacoding.storev2.user;
 
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@RequiredArgsConstructor
+@Repository
 public class UserRepository {
+    private final EntityManager em;
 }
